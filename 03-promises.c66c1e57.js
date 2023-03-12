0c0f9d8fb150659c064a643d21fd3719cf3ee6b1
PiotrKoczkodaj <101861936+PiotrKoczkodaj@users.noreply.github.com>
2023-03-12 16:05:19 +0000

    Deploying to gh-pages from @ PiotrKoczkodaj/goit-js-hw-09-2@6c69df9eb19eb22912f0604bc57f115d0afd0778 🚀

---
+++ 03-promises.c66c1e57.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".form"),t=(document.querySelector('input[name="delay"]'),document.querySelector('input[name="step"]'));document.querySelector('input[name="amount"]');console.log(t),e.addEventListener("submit",(function(e,t){Math.random()}));
+//# sourceMappingURL=03-promises.c66c1e57.js.map
