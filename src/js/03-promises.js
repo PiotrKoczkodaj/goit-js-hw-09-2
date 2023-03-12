6c69df9eb19eb22912f0604bc57f115d0afd0778
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-03-12 17:04:56 +0100

    .

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,20 @@
+const form = document.querySelector('.form');
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+console.log(step)
+
+
 function createPromise(position, delay) {
+  
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+
   } else {
     // Reject
+   
   }
 }
+
+form.addEventListener('submit',createPromise)
