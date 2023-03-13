691b605c6bbc6fe6ebe77b3b5e22927c94825f2f
PiotrKoczkodaj <101861936+PiotrKoczkodaj@users.noreply.github.com>
2023-03-13 21:28:24 +0000

    Deploying to gh-pages from @ PiotrKoczkodaj/goit-js-hw-09-2@5adf2c4806f7d72aa21be5bade127207050c7ec2 🚀

--- 03-promises.3a93f776.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".form"),n=(document.querySelector('input[name="delay"]'),document.querySelector('input[name="step"]'));document.querySelector('input[name="amount"]');console.log(n),e.addEventListener("submit",(function(e,n){Math.random()}))}();
-//# sourceMappingURL=03-promises.3a93f776.js.map
---
+++ 03-promises.e4fa65cf.js
@@ -0,0 +1,2 @@
+document.querySelector(".form");const e=document.querySelector('input[name="delay"]').value;document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]');console.log(e),function(e,o){const n=Math.random()>.3;new Promise(((t,l)=>{setInterval((()=>{n?t(`✅ Fulfilled promise ${obj.position} in ${obj.delay}ms`):l(`❌ Rejected promise ${e} in ${o}ms`)}),o)})).then((e=>{console.log(e)})).then((e=>{console.log(e)}))}(1,1e3);
+//# sourceMappingURL=03-promises.e4fa65cf.js.map
