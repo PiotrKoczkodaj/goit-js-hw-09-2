723d0dc1c2655658701eef36010116aeccef59a7
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-03-15 02:09:30 +0100

    .

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,31 +1,67 @@
 const form = document.querySelector('.form');
-const delay = document.querySelector('input[name="delay"]').value;
-const step = document.querySelector('input[name="step"]');
-const amount = document.querySelector('input[name="amount"]');
-console.log(delay)
+const delayInput = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]').value;
+const amountInput = document.querySelector('input[name="amount"]');
+const btn = document.querySelector('button[type="submit"]');
 
 
-function createPromise(position,delay) {
-  
-  const shouldResolve = Math.random() > 0.3;
 
+function createPromise(e,position,delay) {
+  e.preventDefault();
+  delay = delayInput.value;
+  amount = amountInput.value;
+
+  let interval = setInterval(() => {
+    const shouldResolve = Math.random() > 0.3;
+    
   const promise = new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      reject(`❌ Rejected promise ${position} in ${delay}ms`)
+    }
+  });
 
-       setInterval(() => {
-  if (shouldResolve) {
-    resolve(`✅ Fulfilled promise ${obj.position} in ${obj.delay}ms`);
-  } else {
-    reject(`❌ Rejected promise ${position} in ${delay}ms`)
+    promise.then(({ position, delay }) => {
+
+      const positionRendering = () => {
+        
+        for (let i = 1; i<=amount; i++) {
+          return i
+        }
+      };
+
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  }).catch(({position,delay}) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+},delay)
   }
 
-  },delay)
+form.addEventListener('submit',createPromise)
 
-  }).then(resolve => {
-    console.log(resolve)
-  }).then(reject => {
-    console.log(reject)
-  })
-}
+// setInterval(() => {
+// const isSuccess = Math.random() > 0.3;
+// const promise = new Promise((resolve, reject) => {
+  
+//     if (isSuccess) {
+//       resolve("Success! Value passed to resolve function");
+//     } else {
+//       reject("Error! Error passed to reject function");
+//     }
+//   });
 
-// form.addEventListener('click',createPromise)
-createPromise(1,1000)
+
+// promise.then(
+//   // onResolve will run third or not at all
+//   value => {
+    
+//     console.log(value); // "Success! Value passed to resolve function"
+//   },
+//   // onReject will run third or not at all
+//   error => {
+    
+//     console.log(error); // "Error! Error passed to reject function"
+//   }
+// )},1000);
